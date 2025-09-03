04d8849343c6cca6444ffe0a296a81db58bcebca
Luke <lukebardini02@gmail.com>
2025-09-03 20:28:43 -0300

    n

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 print("Hello World")
 
-a=1
+a=print(int(input("valor a:")))
+b=print(int(input("valor b:")))
 
-
-print(input("valor b:"))
-
-if b> a:
+if b > a:
     print("b é maior que a")
 else:
     print('b é menor que a')
